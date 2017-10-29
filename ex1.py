2cfe976a872400e2f2d5521b9883419c425fa38d
kangdalei <kangdalei@foxmail.com>
2017-10-29 16:33:00 +0800

    2017.10.9 add

---
+++ ex1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fan"
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "sadi" do not touch this.'
+print '===========================' 
+print u"你好，世界"
+print u"再次你好"
+print u"这很有趣"
+print u'耶！打印.'
+print u"我宁愿你不要"
+print u'我说不要碰这个.'
