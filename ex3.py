343a302eb1a7147abec7d194bea0e45ea10185a2
kangdalei <kangdalei@foxmail.com>
2017-11-05 21:42:47 +0800

    add ex3 2017.11.05

---
+++ ex3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+print u"我来算算我的小鸡:"
+
+print u"母鸡", 25 + 30/6
+print u"公鸡", 100 - 25 * 3 % 4
+
+print 3+2+1-5+4%2-1/4+6
+
+print "Is it greater?", 5 > -2
+
+print "What is 3+2?", 3+2
+
+
